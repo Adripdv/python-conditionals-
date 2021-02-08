7d4a6b09ee7f87ebc9ee1f79e9924bdc88234524
Adriana Oliveira <adripdv@gmail.com>
2021-02-08 15:00:55 +0000

    Lesson completed

---
+++ main.py
@@ -0,0 +1,59 @@
+# x = 15
+# y = 7
+
+# if x > y:
+#   print('yes')
+
+#==============================================
+
+# x = 5
+# y = 7
+
+# if x < y:
+#   print('No')
+
+# if 'Hello' in 'Hello world':
+#   print('yes')
+
+#==============================================
+
+# x = 7
+# y = 7
+
+# if x > y:
+#     print('yes it is greater')
+# elif x < y:
+#     print('no it is less')
+# else:
+#     print('Neither')
+
+
+'''Nested if statments'''
+
+# num = -3
+
+# if num >= 0:
+#   if num == 0:
+#     print('Zero')
+#   else:
+#     print('Positive number')
+# else:
+#   print('Negative number')
+
+
+''' one line if statments'''
+
+# if 7 > 5: print('Yes it is')
+
+# if 7 > 5:
+#   print('Yes it is')
+
+'''ternary operator'''
+
+# print('Yes') if 7 > 9 else print('no')
+
+#==============================================
+
+# if 100 > 50:
+#   pass
+
